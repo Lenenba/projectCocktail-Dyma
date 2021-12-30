4b5f0e76036fe17124b6882082111514002e3c18
Lenenba <bilitikjulesroger@yahoo.fr>
2021-12-30 15:55:10 -0500

    cocktails

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Cocktail } from './shared/interfaces/cocktail.interface';
+import { CocktailService } from './shared/services/cocktail.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor( private cocktailService : CocktailService){}
+
+  ngOnInit(): void {
+    this.cocktailService.getAllCocktails().subscribe()
+  }
+}
